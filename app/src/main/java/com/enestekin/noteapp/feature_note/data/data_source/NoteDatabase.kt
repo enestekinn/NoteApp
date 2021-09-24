a6784275a5ed9def17effb52c4262aae0c9f2d88
enestekin <enestekin_@hotmail.com>
2021-09-24 17:08:24 +0300

    hilt-setup

--- app/src/main/java/com/enestekin/noteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/enestekin/noteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -13,4 +13,8 @@ import com.enestekin.noteapp.feature_note.domain.model.Note
 abstract class NoteDatabase: RoomDatabase() {
 
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
